a78f1b0a644d02fa9ea9d18fafacc95f8d0d5c5e
Christian Dullweber <xchrdw@gmail.com>
2014-03-18 13:52:38 +0100

    test piwik

--- js/grayscale.js
+++ js/grayscale.js
@@ -22,3 +22,16 @@ $(function() {
     });
 });
 
+
+<!-- Piwik -->
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.chrdw.de/";
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', 1]);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+<!-- End Piwik Code -->
